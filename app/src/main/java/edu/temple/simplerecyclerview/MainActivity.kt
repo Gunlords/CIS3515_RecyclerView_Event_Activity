1a3c4db60c9d50bac0cb0472f2fec7ac37628d10
Gunlords <tur68285@temple.edu>
2025-02-13 12:12:54 -0500

    lamda function modifies displaysize, pass lamda to adapter

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -1,3 +1,5 @@
+
+
 package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
@@ -11,18 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Set up RecyclerView display responsibility
-        val numbers = Array(100) {it + 1}
+        val numbers = Array(100) { it + 1 }
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val displayTextView = findViewById<TextView>(R.id.displayTextView)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
+
         // TODO (Step 2: Define lambda to modify displayTextView size)
+        // Define lambda to modify displayTextView
+        val callBack: (Int) -> Unit = { size ->
+            displayTextView.textSize = size.toFloat()
+            displayTextView.text = "Text Size: $size"
+        }
+
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
-
-
+        // Pass lambda to adapter
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callBack)
     }
-
 }
